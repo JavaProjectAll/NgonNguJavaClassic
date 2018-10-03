3655f6ce062640915fa76b70aca660a558c44736
= <=>
2018-10-03 09:51:03 +0700

    bai2 khai bao su dung bien

---
+++ src/bai2_Bien.java
@@ -0,0 +1,16 @@
+public class bai2_Bien {
+    public static void main(String[] args){
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.7;
+        char c = 'a';
+        String s = "Hà Nội";
+        boolean b = true;
+        System.out.println("i = "+i);
+        System.out.println("f = "+f);
+        System.out.println("d = "+d);
+        System.out.println("c = "+c);
+        System.out.println("s = "+s);
+        System.out.println("b = "+b);
+    }
+}
