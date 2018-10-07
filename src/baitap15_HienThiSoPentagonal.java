f300df2ce80474f9342388ccc3c47f67dc744233
= <=>
2018-10-07 22:15:31 +0700

    bài tập số pentagonal

--- src/baitap15_HienThiSoPentagonal.java
+++ src/baitap15_HienThiSoPentagonal.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
+
 public class baitap15_HienThiSoPentagonal {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập 1 số kiểm tra Pentagonal");
+        int number = sc.nextInt();
+        boolean check = isPentagonalNumber(number);
+        if (check) {
+            System.out.println(number + " là số Pentagonal");
+        } else {
+            System.out.println(number + " không phải số Pentagonal");
+        }
     }
 
-    public static int getPentagonalNumber(int n){
-        int penta = 0;
-        if (n*(3*n - 1)/2 == 0){
-
+    public static boolean isPentagonalNumber(int n) {
+        if (n > 0) {
+            int m;
+            int i = 0;
+            do {
+                m = (3 * i * i - i) / 2;
+                i++;
+            } while (m < n);
+            return (m == n);
         }
-        return 0;
+        return false;
     }
 }
